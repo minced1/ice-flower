cc5350b6062d17cbdc3e56c1d969b5fc4420f1f6
minced1 <n.gude@proton.me>
2023-09-30 18:57:46 +0200

    update

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "Never's Flake";
+  description = "ice-flower: Never's Flake";
   
   inputs = {
     nixpkgs.url = "nixpkgs/nixos-unstable";
@@ -16,13 +16,13 @@
       nixos = nixpkgs.lib.nixosSystem {
       	system = "x86_64-linux";
         modules = [
-          ./system/configuration.nix
+          ./system/default.nix
 
           home-manager.nixosModules.home-manager {
             home-manager.useGlobalPkgs = true;
             home-manager.useUserPackages = true;
             home-manager.users.never = import ./users/never/home.nix;
-	  }
+					}
 
           musnix.nixosModules.musnix
         ];
