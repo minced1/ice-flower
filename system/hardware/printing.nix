504e1c5b4b101a5be68fc897515088367f1b87a8
minced1 <n.gude@proton.me>
2023-10-13 10:19:35 +0200

    configure printing

---
+++ system/hardware/printing.nix
@@ -0,0 +1,14 @@
+{ config, lib, pkgs, ... }:
+{  
+  environment.systemPackages = with pkgs; [
+      epson-escpr
+  ];
+  
+  # Enable CUPS to print documents.
+  services.printing = {
+    enable = true;
+    drivers = [ pkgs.epson-escpr ]
+  };
+}
+ 
+
