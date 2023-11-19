d2c1c74b648b7d8203680e68e5623faa483e3c24
minced1 <n.gude@proton.me>
2023-11-19 15:08:04 +0100

    add kde-plasma

--- modules/kde-plasma/default.nix
+++ modules/kde-plasma/default.nix
@@ -1,34 +1,20 @@
 { config, lib, pkgs, ... }:
 {
+	# Enable the X11 windowing system.
+	services.xserver.enable = true;
+	# services.xserver.displayManager.defaultSession = "plasmawayland";
 	# Enable the GNOME Desktop Environment.
 	services.xserver.displayManager.sddm.enable = true;
 	services.xserver.desktopManager.plasma5.enable = true;
 
-	environment.systemPackages = with pkgs; [
-		fragments
-		gnome.gnome-software
-		gnome-builder
-		gnome.dconf-editor
-		#celeste
-
-		gnomeExtensions.rounded-window-corners
+	environment.plasma5.excludePackages = with pkgs.libsForQt5; [
+		# elisa
+		# gwenview
+		# okular
+		# oxygen
+		# khelpcenter
+		# konsole
+		# plasma-browser-integration
+		# print-manager
 	];
-
-	programs.kdeconnect = {
-		enable = true;
-		package = pkgs.gnomeExtensions.gsconnect;
-	};
-	environment.gnome.excludePackages = (with pkgs; [
-		gnome-photos
-		gnome-tour
-		gnome-connections
-	]) ++ (with pkgs.gnome; [
-		cheese
-		gnome-music
-		totem
-		simple-scan
-		baobab
-		file-roller
-		seahorse
-	]);
 }
