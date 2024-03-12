a5b5bd42e5b332ca30ef178978d2551f68729688
minced1 <n.gude@proton.me>
2024-03-12 20:31:43 +0100

    ardour and plugins added

--- modules/audio/music-production.nix
+++ modules/audio/music-production.nix
@@ -5,15 +5,18 @@
 	musnix.enable = true;
 
 	environment.systemPackages = with pkgs; [
-		# ardour
-		# lsp-plugins
+		ardour
+		lsp-plugins
 		# calf
-		# sfizz
-		# drumgizmo
-		# distrho
+		sfizz
+		drumgizmo
+		distrho
 		# airwindows-lv2
-		# x42-plugins
-		# mda_lv2
-		# setbfree
+		x42-plugins
+		x42-avldrums
+		mda_lv2
+		setbfree
+		neural-amp-modeler-lv2
+		# surge-XT
 	];
 }
